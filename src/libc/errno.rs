48a79fb7577cdfcd3c9f36710f232df3127aa6c8
5-pebbles <me@auxv.org>
2025-10-17 17:18:31 -0400

    feat(errno): implement errno;

---
+++ src/libc/errno.rs
@@ -0,0 +1,19 @@
+use std::cell::Cell;
+
+#[repr(transparent)]
+#[doc(alias = "errno")]
+pub struct Errno(pub(crate) u32);
+
+thread_local! {
+    #[no_mangle]
+    #[allow(non_upper_case_globals)]
+    pub static errno: Cell<Errno> = Cell::new(Errno(0));
+}
+
+pub fn set_errno(new_errno: Errno) {
+    errno.with(|e| e.set(new_errno));
+}
+
+impl Errno {
+    pub const BADF: Self = Self(linux_raw_sys::errno::EBADF);
+}
