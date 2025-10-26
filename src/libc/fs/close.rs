6d624801cd4ee31813666e094661e064795a617f
5-pebbles <me@auxv.org>
2025-10-26 13:18:47 -0400

    fix(libc): change up `close` to check fd validity;

--- src/libc/fs/close.rs
+++ src/libc/fs/close.rs
@@ -1,16 +1,20 @@
 use std::{arch::asm, os::fd::RawFd};
 
-use crate::libc::errno::{set_errno, Errno};
+use crate::{
+    libc::errno::{set_errno, Errno},
+    signature_matches_libc,
+};
 
 #[no_mangle]
 unsafe extern "C" fn close(file_descriptor: RawFd) -> i32 {
-    let result: isize;
+    signature_matches_libc!(libc::close(file_descriptor));
 
     if file_descriptor == -1 {
         set_errno(Errno::BADF);
         return -1;
     }
 
+    let result: isize;
     #[cfg(target_arch = "x86_64")]
     {
         const CLOSE: usize = 3;
