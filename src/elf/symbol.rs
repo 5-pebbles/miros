665caa3388a4c2ffa0d023984d9a98fea20e6237
5-pebbles <me@auxv.org>
2025-12-01 12:33:08 -0500

    feat(elf): use bitfield in symbol .st_other field;

--- src/elf/symbol.rs
+++ src/elf/symbol.rs
@@ -1,13 +1,30 @@
+use bitbybit::{bitenum, bitfield};
+
+#[bitenum(u2, exhaustive = true)]
+pub enum SymbolVisibility {
+    Default = 0b00,
+    Internal = 0b01,
+    Hidden = 0b10,
+    Protected = 0b11,
+}
+
+#[bitfield(u8)]
+pub struct SymbolOtherField {
+    #[bits(0..=1, rw)]
+    symbol_visibility: SymbolVisibility,
+}
+
 #[repr(C)]
 #[derive(Clone, Copy)]
 pub struct Symbol {
+    /// String table index of the symbol name.
     pub st_name: u32,
     #[cfg(target_pointer_width = "32")]
     pub st_value: usize,
     #[cfg(target_pointer_width = "32")]
     pub st_size: usize,
     pub st_info: u8,
-    pub st_other: u8,
+    pub st_other: SymbolOtherField,
     pub st_shndx: u16,
     #[cfg(target_pointer_width = "64")]
     pub st_value: usize,
