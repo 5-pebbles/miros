b762980e3154983ff0f4c34cc07ff645f9b86791
5-pebbles <me@auxv.org>
2026-03-17 06:51:12 -0400

    feat(printf): add end-to-end sprintf integration tests;

--- src/libc/str/printf/mod.rs
+++ src/libc/str/printf/mod.rs
@@ -36,3 +36,65 @@ unsafe fn write_formatted_to_file_descriptor(
 
     formatter.finish()
 }
+
+#[cfg(test)]
+#[allow(improper_ctypes_definitions)]
+mod tests {
+    use std::ffi::CString;
+
+    use super::*;
+
+    unsafe extern "C" fn sprintf(format: *const i8, mut args: ...) -> Vec<u8> {
+        let mut output = Vec::new();
+        {
+            let mut formatter = Formatter::new(&mut output);
+            for item in parse::PrintfParser::new(format) {
+                match item {
+                    PrintfItem::Literal(bytes) => formatter.write_bytes(bytes),
+                    PrintfItem::Specifier(spec) => {
+                        let resolved = ResolvedSpecifier::from_parsed(spec, &mut args.as_va_list());
+                        formatter.format(&resolved, &mut args.as_va_list());
+                    }
+                }
+            }
+        }
+        output
+    }
+
+    macro_rules! sprintf_tests {
+        (mod $mod_name:ident {
+            $($name:ident, $format:expr, ($($arg:expr),*), $expected:expr);* $(;)?
+        }) => {
+            mod $mod_name {
+                use super::*;
+                $(
+                    #[test]
+                    fn $name() {
+                        let format =
+                            CString::new($format).expect("format should contain no internal nulls");
+                        let output = unsafe { sprintf(format.as_ptr(), $($arg),*) };
+                        assert_eq!(output, $expected.as_bytes());
+                    }
+                )*
+            }
+        };
+    }
+
+    sprintf_tests!(mod end_to_end {
+        pure_literal,        "hello",                    (),                                     "hello";
+        percent_escape,      "100%%",                    (),                                     "100%";
+        signed_int,          "%d",                       (42i32),                                "42";
+        negative_int,        "%d",                       (-42i32),                               "-42";
+        multiple_specifiers, "%d + %d = %d",             (1i32, 2i32, 3i32),                     "1 + 2 = 3";
+        star_width,          "%*d",                      (10i32, 42i32),                         "        42";
+        star_precision,      "%.*d",                     (5i32, 42i32),                          "00042";
+        string,              "%s",                       (b"hello\0".as_ptr() as *const i8),     "hello";
+        char_format,         "%c",                       (65i32),                                "A";
+        null_pointer,        "%p",                       (core::ptr::null::<()>()),              "(nil)";
+        hex_alternate,       "%#x",                      (255u32),                               "0xff";
+        padded_string,       "[%-10s]",                  (b"hi\0".as_ptr() as *const i8),        "[hi        ]";
+        zero_padded_int,     "%08d",                     (42i32),                                "00000042";
+        forced_sign,         "%+d",                      (42i32),                                "+42";
+        literal_and_specs,   "val=%d hex=%#x",           (42i32, 255u32),                        "val=42 hex=0xff";
+    });
+}
