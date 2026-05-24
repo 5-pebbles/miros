c7ffc741b9980310525af4aa593d36de95730ed0
5-pebbles <me@auxv.org>
2026-05-24 16:17:31 -0400

    feat(tls): implement ModuleRegistry with generation-aware iteration;

--- src/tls/module_registry.rs
+++ src/tls/module_registry.rs
@@ -1,10 +1,38 @@
-pub struct ThreadLocalAllocation {
-    module_id: usize,
-    block_offset: usize,
-    // This enables lazy per-thread allocation when thread.generation < self.generation & modules are dlopen'd after threads are created
-    generation: usize,
+use crate::{tls::template::TlsTemplate, utils::mremap_allocator::MremapAllocator};
+
+pub struct ModuleAllocation {
+    pub block_offset: isize,
+    pub template: TlsTemplate,
+    pub generation: usize,
 }
 
-pub struct ModuleRegistry {}
+pub struct ModuleRegistry {
+    modules: Vec<ModuleAllocation, MremapAllocator>,
+}
 
-impl ModuleRegistry {}
+impl ModuleRegistry {
+    pub fn new() -> Self {
+        Self {
+            modules: Vec::new_in(MremapAllocator),
+        }
+    }
+
+    pub fn push(&mut self, allocation: ModuleAllocation) {
+        self.modules.push(allocation);
+    }
+
+    pub fn get(&self, module_id: usize) -> &ModuleAllocation {
+        &self.modules[module_id]
+    }
+
+    pub fn since(&self, generation: usize) -> impl Iterator<Item = (usize, &ModuleAllocation)> {
+        self.modules
+            .iter()
+            .enumerate()
+            .filter(move |(_, module)| module.generation > generation)
+    }
+
+    pub fn count(&self) -> usize {
+        self.modules.len()
+    }
+}
